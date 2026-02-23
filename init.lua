ea4adf4cef7508ca72f3e48c6b7dedac6999c4f3
smalinow <stanislaw.malinowski@imc.com>
2026-02-23 09:37:56 +0100

    removed Tab bind as <Tab>==<C-i>

--- init.lua
+++ init.lua
@@ -216,9 +216,6 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
-vim.keymap.set('n', '<Tab>', ']b', { desc = 'Show next buffer' })
-vim.keymap.set('n', '<Shift-Tab>', ']B', { desc = 'Show prev buffer' })
-
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
